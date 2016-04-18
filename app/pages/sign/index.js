4cc5bf869e76332126e42d6fa17722d97d9b4c90
atticussterman <atticus.sterman@gmail.com>
2016-04-18 17:18:02 -0400

    message signing

--- app/pages/sign/index.js
+++ app/pages/sign/index.js
@@ -3,6 +3,7 @@
 var Ractive = require('hive-ractive')
 var emitter = require('hive-emitter')
 var getWallet = require('hive-wallet').getWallet
+var bitcoin = require('bitcoinjs-lib')
 
 module.exports = function(el){
     var ractive = new Ractive({
@@ -14,12 +15,32 @@ module.exports = function(el){
 
     emitter.on('balance-ready', function() {
         var wallet = getWallet();
-        console.log(wallet);
         var currentAddr = wallet.getNextAddress();
         var addresses = wallet.addresses.slice();
         addresses.push(currentAddr);
-
         ractive.set('addresses', addresses);
+    });
+
+    ractive.on('clear-signature', function() {
+        ractive.set('signature', false);
+    });
+
+    ractive.on('sign-message', function(event) {
+        var address = event.node.childNodes[2].innerHTML;
+        var privKey;
+        var wallet = getWallet();
+        if (address === wallet.getNextAddress()) {
+            privKey = wallet.externalAccount.derive(wallet.addresses.length).privKey;
+        } else {
+            privKey = wallet.getPrivateKeyForAddress(address);
+        }
+        var message = ractive.get('message');
+        var sig = bitcoin.Message.sign(privKey, message).toString('base64');
+        ractive.set('signature', sig);
+        console.log("bitcoin-cli verifymessage '" +
+                    address + "' '" +
+                    sig + "' '" +
+                    message + "'");
     });
 
     return ractive
